759147a277dff8509fdfab4669c0096d373f6819
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-02-25 01:54:35 +0300

    sleepy dude fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    port: 5173
+  },
   build: {
     rollupOptions: {
       external: [
-        // Match these exactly with your component imports
         '@mui/x-data-grid/theme/material/styles.css'
       ]
     }
