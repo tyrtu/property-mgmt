c73d0499716f7f3d6ac703691098e3f2b1eff640
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-02-25 01:36:10 +0300

    sleepy dude fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcssImport from 'postcss-import';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
   },
-  css: {
-    postcss: {
-      plugins: [
-        postcssImport() // Add CSS import resolver
-      ]
-    }
-  },
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true // Fix MUI module resolution
-    },
-    rollupOptions: {
-      external: [
-        '@mui/x-data-grid/theme/material/styles.css' // Allow CSS imports
-      ]
+      transformMixedEsModules: true
     }
   }
 });
