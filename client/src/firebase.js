9074a448a6093491014c76a119a1128b9ebf53ce
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-03-06 19:00:14 +0300

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyAiDVXvvhDfm6aa47ixWDwb-mzWK8g7l3A",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
