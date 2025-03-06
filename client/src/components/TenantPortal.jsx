54aa338622d3cad85d79d0eb5c91a4913d59ab12
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-03-06 15:19:13 +0300

    Update TenantPortal.jsx

--- client/src/components/TenantPortal.jsx
+++ client/src/components/TenantPortal.jsx
@@ -1,6 +1,4 @@
-// src/components/TenantPortal.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import TenantLogin from './TenantLogin';
 import TenantDashboard from './TenantDashboard';
 import TenantPaymentHistory from './TenantPaymentHistory';
@@ -10,18 +8,15 @@ import TenantProfile from './TenantProfile';
 
 const TenantPortal = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/tenant/login" element={<TenantLogin />} />
-        <Route path="/tenant/dashboard" element={<TenantDashboard />} />
-        <Route path="/tenant/payments" element={<TenantPaymentHistory />} />
-        <Route path="/tenant/maintenance" element={<TenantMaintenance />} />
-        <Route path="/tenant/notifications" element={<TenantNotifications />} />
-        <Route path="/tenant/profile" element={<TenantProfile />} />
-        {/* Redirect any unknown route to the login page */}
-        <Route path="*" element={<Navigate to="/tenant/login" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/tenant/login" element={<TenantLogin />} />
+      <Route path="/tenant/dashboard" element={<TenantDashboard />} />
+      <Route path="/tenant/payments" element={<TenantPaymentHistory />} />
+      <Route path="/tenant/maintenance" element={<TenantMaintenance />} />
+      <Route path="/tenant/notifications" element={<TenantNotifications />} />
+      <Route path="/tenant/profile" element={<TenantProfile />} />
+      <Route path="*" element={<Navigate to="/tenant/login" />} />
+    </Routes>
   );
 };
 
