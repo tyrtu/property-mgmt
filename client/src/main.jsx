7c63a2eb14e675165a72d9c97e8d10ca0fb7f640
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-03-14 00:56:26 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import '@fontsource/roboto/700.css';
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('client/pwabuilder-sw.js')
+      .register('/pwabuilder-sw.js')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
