8ab0864a7825d83f9099e14dfe9e86c9a397de4c
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-03-14 01:52:45 +0300

    mm

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import '@fontsource/roboto/700.css';
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/pwabuilder-sw.js')
+      .register('dist/assets/pwabuilder-sw.js')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
