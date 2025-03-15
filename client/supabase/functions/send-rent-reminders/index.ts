51024c03a3864a7d14dccdce7e377d114ba55574
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-03-15 11:42:14 +0300

    Update index.ts

--- client/supabase/functions/send-rent-reminders/index.ts
+++ client/supabase/functions/send-rent-reminders/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import twilio from "https://esm.sh/twilio@4.23.0"; // Use a specific version of Twilio
+import twilio from "https://esm.sh/twilio@4.23.0"; // Use a specific version
 
 // Load environment variables
 const supabaseUrl = Deno.env.get("SUPABASE_URL");
