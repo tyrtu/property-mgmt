929b9239985d3fc3388d1dde3d755a702a8ae835
Raphael Kenyoru <kenyoruraphael21@gmail.com>
2025-03-26 15:02:04 +0300

    Update TenantRegister.jsx

--- client/src/components/TenantRegister.jsx
+++ client/src/components/TenantRegister.jsx
@@ -49,6 +49,7 @@ const TenantRegister = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+  const { signup, signInWithGoogle } = useAuth();
 
   useEffect(() => {
     const fetchProperties = async () => {
